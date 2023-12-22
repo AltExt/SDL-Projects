c8407ee35227a1e617a1a9f18aeed7a5dc75c4f8
AltExt <52500808+AltExt@users.noreply.github.com>
2023-12-22 13:43:07 +0000

    cpp works, it lives!!!!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <vector>
 
 // this is a comment
 
 int main(int argc, const char* argv[]) {
+	std::vector<int> ints = {0, 1, 2, 3, 4, 5};
 	std::cout << "This is the output!!" << std::endl;
+	for (int i = 0; i < ints.size(); i++) {
+		std::cout << "ints: " << ints[i] << std::endl;
+	}
 	return 0;
 }
